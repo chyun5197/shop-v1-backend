f203cd8c922ae6ff522fb5be57ffdb3356961724
chyun5197 <103193121+chyun5197@users.noreply.github.com>
2025-02-20 16:19:46 +0900

    분류번호작업

--- src/main/java/project/shopclone/domain/product/controller/CrawlingController.java
+++ src/main/java/project/shopclone/domain/product/controller/CrawlingController.java
@@ -3,6 +3,7 @@ package project.shopclone.domain.product.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import project.shopclone.domain.product.service.Crawling;
 import project.shopclone.domain.product.entity.Brand;
@@ -25,13 +26,13 @@ public class CrawlingController {
     // => 실행 오래 걸리더라도 정확하게끔, 나중에 코드 확장하기 편하게 알아보기 쉽도록 정리
 
     // 모든 브랜드 모든 상품의 정보 저장 (상세 이미지 제외) | 완료: Guitar1,
-    @GetMapping("/crawling")
-    public String crawl() {
+    @GetMapping("/crawling/{password}")
+    public String crawl(@PathVariable String password) {
+        if (!password.equals(pw)) {
+            return "접근 불가";
+        }
         List<Brand> brands = brandRepository.findAll();
         for (Brand brand : brands){
-            if (brand.getId()<169){
-                continue;
-            }
             crawling.crawlingPages(brand.getCateNo(), brand.getBrand(), brand.getCategory());
             // 분류번호 작업
 //            crawling.cateNumbering(brand.getCateNo(), brand.getBrand(), brand.getCategory());
@@ -39,17 +40,22 @@ public class CrawlingController {
         return "성공";
     }
 
-    @GetMapping("/numbering")
-    public void numbering() {
+    // 분류번호작업
+    @GetMapping("/numbering/{password}")
+    public String numbering(@PathVariable String password) {
+        if (!password.equals(pw)) {
+            return "접근 불가";
+        }
         List<Brand> brands = brandRepository.findAll();
         for (Brand brand : brands){
-            if (brand.getCategory().equals("Bass 2")) {
-                break;
+            if (brand.getId() < 53) {
+                continue;
             }
 
             // 분류번호 작업
             crawling.cateNumbering(brand.getCateNo(), brand.getBrand(), brand.getCategory());
         }
+        return "성공";
     }
 
 //    @GetMapping("/delete")
