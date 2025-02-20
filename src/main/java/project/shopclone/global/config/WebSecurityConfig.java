6a18b4ba552fbac7c4afcfafad437961336881eb
chyun5197 <103193121+chyun5197@users.noreply.github.com>
2025-02-21 01:24:37 +0900

    스프링 cors 제거

--- src/main/java/project/shopclone/global/config/WebSecurityConfig.java
+++ src/main/java/project/shopclone/global/config/WebSecurityConfig.java
@@ -139,19 +139,19 @@ public class WebSecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public CorsFilter corsFilter() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:5174");// 리액트 서버
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-
-        return new CorsFilter(source);
-    }
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.setAllowCredentials(true);
+//        config.addAllowedOrigin("http://localhost:5174");// 리액트 서버
+//        config.addAllowedHeader("*");
+//        config.addAllowedMethod("*");
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", config);
+//
+//        return new CorsFilter(source);
+//    }
 
     @Bean
     public TokenAuthenticationFilter tokenAuthenticationFilter() {
