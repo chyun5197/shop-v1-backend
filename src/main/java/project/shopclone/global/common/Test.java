3ca2a0606a7597e2319341f3b626f26a13c45c5a
chyun5197 <103193121+chyun5197@users.noreply.github.com>
2025-02-27 15:27:54 +0900

    로드밸런싱

--- src/main/java/project/shopclone/global/common/Test.java
+++ src/main/java/project/shopclone/global/common/Test.java
@@ -1,15 +1,18 @@
 package project.shopclone.global.common;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 public class Test {
-    @GetMapping("test")
-    public String test() {
-        return "test";
+    // 로드밸런싱으로 서버가 바뀌면 응답도 바뀌도록
+    public static final UUID randomUUID = UUID.randomUUID();
+
+    @GetMapping("/test/balancer")
+    public ResponseEntity<UUID> test() {
+        return ResponseEntity.ok(randomUUID);
     }
-
-
 }
