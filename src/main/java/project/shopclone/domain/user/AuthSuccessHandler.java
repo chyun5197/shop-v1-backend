7dea6fe348afc4ce5c98e8b22e6541bd6c70f99e
chyun5197 <103193121+chyun5197@users.noreply.github.com>
2025-04-03 11:41:04 +0900

    주문조회

--- src/main/java/project/shopclone/domain/user/AuthSuccessHandler.java
+++ src/main/java/project/shopclone/domain/user/AuthSuccessHandler.java
@@ -1,7 +1,6 @@
-package project.shopclone.domain.user.controller;
+package project.shopclone.domain.user;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
