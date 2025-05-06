2253c1b8445f7f11b4521974c9cfcb5a15c8d189
chyun5197 <103193121+chyun5197@users.noreply.github.com>
2025-05-06 15:37:47 +0900

    Websocket + Stomp + mongoDB / yml 수정 반영

--- src/main/java/project/shopclone/domain/chat/document/ChatMessage.java
+++ src/main/java/project/shopclone/domain/chat/document/ChatMessage.java
@@ -19,7 +19,7 @@ public class ChatMessage {
     private String id;
 
     private Long chatRoomId;
-    private String role;                // 역할(상담사 or 고객)
+    private String role;                // 역할 (상담사, 고객)
     private Long memberId;              // 메시지 보낸 멤버 id
     private String message;             // 메시지 내용
     private LocalDateTime timestamp;    // 메시지 전송 시간
