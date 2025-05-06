1995e29edfe3a75c6dc746b4b30d708d7dc12959
chyun5197 <103193121+chyun5197@users.noreply.github.com>
2025-05-06 22:37:20 +0900

    web, messaging 로그 상세 설정

--- src/main/java/project/shopclone/domain/chat/dto/ChatMessageDto.java
+++ src/main/java/project/shopclone/domain/chat/dto/ChatMessageDto.java
@@ -9,7 +9,7 @@ import project.shopclone.domain.member.entity.Role;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class ChatMessageDto {
+public class ChatMessageDto { //
     private Long chatRoomId;
     private Long memberId;
     private String role;
