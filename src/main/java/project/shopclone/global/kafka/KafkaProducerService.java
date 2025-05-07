d05849aab46dc7e896761749e8bd6ceebc46225d
chyun5197 <103193121+chyun5197@users.noreply.github.com>
2025-05-07 18:02:52 +0900

    kafka 설정

--- src/main/java/project/shopclone/global/kafka/KafkaProducerService.java
+++ src/main/java/project/shopclone/global/kafka/KafkaProducerService.java
@@ -1,5 +1,6 @@
 package project.shopclone.global.kafka;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
